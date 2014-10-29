b1ac45b5002b8cd0ef5949ff4710a0c46ee881a1
zxp <qianyepushui_zxp@sina.com>
2014-10-29 17:09:54 +0800

    支持左右方向

--- jquery.widget.js
+++ jquery.widget.js
@@ -7,56 +7,78 @@
 (function(window,$) {
 	 $.Widget = function(options,element){
 	 	   this.element = $(element);
+	 	   this.status = 'closed';
 	 	   this.defaults = {
+					container : window.document.body,
 					speed : 300,                /*滑出速度 ms*/
-					width : window.document.body.clientWidth,   /*widget滑出距离*/
-					noCloseClass : 'widget-noclose',  
-					widgetBodyClass : 'widget-body',
-					onClose : null,
-					showRightToLeft:true
-				};			 
-			
+					distance : window.document.body.clientWidth,   /*widget滑出距离*/
+					clickClose : false,  
+					widgetBodyClass : '.widget-body',
+					onClose : undefined,
+					direction:'left',
+					left:undefined
+				};			 		
 			 this._init(options);
 	 };
 	 $.Widget.prototype = {
 	     _init : function(options){
-	     	   var instance = this;
-	     	  instance.options = $.extend({}, instance.defaults, options || {});	     	 
+	     	  var instance = this;	     	 
+	     	  var opts = instance.options = $.extend({}, instance.defaults, options || {});	
+	     	  if(!opts.container) return alert('container not be null'),!1;    	  	     	  
+	     	  if(opts.direction == 'left' || opts.direction == 'right'){
+	     	  	  opts.container.style.overflowX = 'hidden';
+	     	  	  opts.container.style.overflowY = 'auto'
+	     	      instance.distance =  typeof opts.distance == 'undefined'? opts.container.clientWidth : opts.distance;
+	     	    console.log(instance.distance)
+	     	    var x = instance.x = typeof opts.left == 'undefined'? (opts.direction == 'left' ? instance.distance : -instance.distance) : opts.left;
+	     	      var style = instance.element[0].style;
+		          style.webkitTransform = 'translate('+ x + 'px,0)' + 'translateZ(0)';
+					    style.msTransform =
+					    style.MozTransform =
+					    style.OTransform = 'translateX('+ x + 'px)';   
+	     	  }else{	     	  	     	 
+	     	         alert('direction is not left or right');
+	     	  }   
 	     },
-	     _translate : function(el,speed,width,rightToLeft){     	 
-	     	  var style = el[0].style;			
-	     	  var width = rightToLeft ? '-'+ width : width;
+	     _translate : function(el,speed,x){     	 
+	     	  var style = el[0].style;		
 						  style.webkitTransitionDuration =
 					    style.MozTransitionDuration =
 					    style.msTransitionDuration =
 					    style.OTransitionDuration =
 					    style.transitionDuration = speed + 'ms';
-					    style.webkitTransform = 'translate('+ width + 'px,0)' + 'translateZ(0)';
+					    style.webkitTransform = 'translate('+ x + 'px,0)' + 'translateZ(0)';
 					    style.msTransform =
 					    style.MozTransform =
-					    style.OTransform = 'translateX('+ width + 'px)';
+					    style.OTransform = 'translateX('+ x + 'px)';
 	     },
 	     open : function(){
 	     	    var instance = this;
 	     	    var el = instance.element;
 	     	    var opts = instance.options;
-	         	el.show();
-						instance._translate(el,opts.speed,opts.width,opts.showRightToLeft);
-					    el.unbind('click').bind('click',function(){
-								if(el.hasClass(opts.noCloseClass)) return;
-								    instance.close();
-								    if(opts.onClose) opts.onClose(el);
-							});
-							el.find('.'+opts.widgetBodyClass).unbind('click').click(function (event) {
-						        event.stopPropagation();
-						  });    
+	         	el.show();  
+	         	var x = opts.direction == 'left' ? instance.x - instance.distance : instance.x + instance.distance;
+						instance._translate(el,opts.speed,x);
+						instance.status = 'opened';
+						if(!opts.clickClose)return;
+				    el.unbind('click').bind('click',function(){						
+							    instance.close();
+						});
+						if(opts.widgetBodyClass)
+						el.find(opts.widgetBodyClass).unbind('click').click(function (event) {
+					        event.stopPropagation();
+					  });   						
 		     },
 		     close : function(){
 		     	  var instance = this;
+		     	  instance.status = 'closed';
 	     	    var el = instance.element;
 	     	    var opts = instance.options;
-		     	  instance._translate(el,opts.speed,0,!opts.showRightToLeft);
-					  //setTimeout(function(){el.hide();},opts.speed);
+		     	  instance._translate(el,opts.speed,instance.x);
+		     	  if(opts.onClose) opts.onClose(el);
+		     },
+		     isShow: function(){
+		     	   return this.status == 'opened';
 		     }	
 		 };
 	 $.fn.widget = function(options){		
@@ -66,26 +88,17 @@
 
             // method 
             case 'string':
-                var args = Array.prototype.slice.call(arguments, 1);
-
-								this.each(function () {
-									var instance = $.data(this, 'widget');
-				
-									if (!instance) {
-										// not setup yet
-										// return $.error('Method ' + options + ' cannot be called until Infinite Scroll is setup');
+                if(this.length > 1)return;
+                var args = Array.prototype.slice.call(arguments, 1);                								
+									var instance = $.data(this[0], 'widget');			
+									if (!instance) {										
+										return false;
+									}				
+									if (!$.isFunction(instance[options]) || options.charAt(0) === "_") {									
 										return false;
 									}
-				
-									if (!$.isFunction(instance[options]) || options.charAt(0) === "_") {
-										// return $.error('No such method ' + options + ' for Infinite Scroll');
-										return false;
-									}
-				
-									// no errors!
-									instance[options].apply(instance, args);
-								});
-				
+									return instance[options].apply(instance, args);
+												
 				            break;
 				
 				            // creation 
@@ -113,11 +126,11 @@
 				                }
 				
 				            });
-				
+				            return this;
 				            break;
 				
 				        }
 
-        return this;
+        
 	 };
 })(window,jQuery);
